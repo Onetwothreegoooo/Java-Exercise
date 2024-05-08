cfb5fc7721480488a17513324183bd468fb6afca
fivefive55 <charlieyang930730@gmail.com>
2024-05-08 17:55:24 +0800

    feat : update

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157133\"\n姓名：\"陳竑達\"");
+        System.out.println("\"學號：\"01157113\"\n姓名：\"楊昕諺\"");
     }
 }
