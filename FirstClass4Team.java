17a4dfc7dcad59208a8eef0a0fcc0149d0c9a95c
tazmi1011 <caix49704@gmail.com>
2024-05-08 17:57:22 +0800

    tazmiupdate

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157133\"\n姓名：\"陳竑達\"");
         System.out.println("\"學號：\"01157113\"\n姓名：\"楊昕諺\"");
+        System.out.println("\"學號：\"01157138\"\n姓名：\"蔡軒語\"");
     }
 }
